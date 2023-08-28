b2af89cf98aaaf9bf811e27cf67071fb4546e60d
João Lucas <joaolucaspinheirotorres@gmail.com>
2023-08-28 18:44:35 -0300

    Update

--- src/main/kotlin/br/com/alura/forum/config/JWTUtils.kt
+++ src/main/kotlin/br/com/alura/forum/config/JWTUtils.kt
@@ -16,7 +16,7 @@ import javax.crypto.spec.SecretKeySpec
 class JWTUtils(
     private val usuarioService: UsuarioService
 ) {
-    private val expiration: Long = 60000
+    private val expiration: Long = 60000*60
     private var secretKey: String = "b0f20c40b516be74bcb5bfc29f54887fada89e9100a1a6e6b7118f61d995f53deb4bebcec8af8f790f09da8cb174ea9c1273257be8af1343dac0825494bad488"
     private val key: Key = SecretKeySpec(secretKey.toByteArray(), "HmacSHA512")
 
